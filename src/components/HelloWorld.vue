34be22fae999532c4d68424e54e0a9f44d8ea640
DasBaka <122975603+DasBaka@users.noreply.github.com>
2023-12-15 11:34:51 +0100

    hover effect with konva

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,22 +2,26 @@
   <v-stage :config="configKonva">
     <v-layer>
       <v-rect
-        @mouseover="fillWithRects"
-        :config="configRect">
+        :config="configRect"
+        @mouseenter="showRects()">
       </v-rect>
     </v-layer>
-    <v-layer :id="insideRect"> </v-layer>
+    <v-layer
+      ref="layer"
+      @mouseout="delRects()"></v-layer>
   </v-stage>
 </template>
 
 <script>
+import Konva from 'konva';
+
 var width = 800;
 var height = 800;
 
 export default {
   data() {
     return {
-      insideRect: '#insideRect',
+      layer: null,
       configKonva: {
         width: width,
         height: height,
@@ -25,8 +29,8 @@ export default {
       configRect: {
         x: width / 2,
         y: height / 2,
-        width: width / 2,
-        height: height / 2,
+        width: width / 3,
+        height: height / 3,
         fill: 'red',
         stroke: 'black',
         strokeWidth: 4,
@@ -40,33 +44,51 @@ export default {
         stroke: 'black',
         strokeWidth: 4,
       },
+      rects: '',
     };
   },
   methods: {
-    fillWithRects() {
-      console.log('hello');
-      /*  for (
-        let j = 0;
-        j * this.layeredRect.height < this.configRect.height;
-        j++
+    showRects() {
+      let group = new Konva.Group({
+        clip: {
+          x: this.configRect.x,
+          y: this.configRect.y,
+          width: this.configRect.width,
+          height: this.configRect.height,
+        },
+      });
+      for (
+        let i = 0;
+        i * this.layeredRect.height < this.configRect.height;
+        i++
       ) {
+        let y = i * this.layeredRect.height;
         for (
-          let i = 0;
-          i * this.layeredRect.width < this.configRect.width;
-          i++
+          let j = 0;
+          j * this.layeredRect.width < this.configRect.width;
+          j++
         ) {
-          this.$refs.insideRect.innerHTML += `<v-rect :config={
-          x: ${100 * i},
-          y: ${100 * j},
-          width: 100,
-          height: 50,
-          fill: 'blue',
-          stroke: 'black',
-          strokeWidth: 4,
-        }`;
+          let x = j * this.layeredRect.width;
+          let rect = new Konva.Rect({
+            x: this.configRect.x + x,
+            y: this.configRect.y + y,
+            width: this.layeredRect.width,
+            height: this.layeredRect.height,
+            fill: 'blue',
+            stroke: 'black',
+            strokeWidth: 4,
+          });
+          group.add(rect);
         }
-      } */
+      }
+      this.layer.add(group);
     },
+    delRects() {
+      this.layer.destroyChildren();
+    },
+  },
+  mounted() {
+    this.layer = this.$refs.layer.getNode();
   },
 };
 </script>
